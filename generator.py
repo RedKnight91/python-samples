99c6ecc63ee45e9ba799eb76fdd34ff9ead0a41e
Michael Cazzarolli <michaelcazzarolli@gmail.com>
2021-07-20 12:44:50 +0200

    Added a complex example to generator

--- generator.py
+++ generator.py
@@ -24,6 +24,35 @@ squaredNumbers = squareNumbers(numbers)
 numbersList = list(numbers)
 
 #The list() variables contain all of the values in memory and are much heavier
-printSizeOf('Mem Size:', numbers, 'Previous numbers generator')
-printSizeOf('Mem Size:', squaredNumbers, 'Square numbers generator')
-printSizeOf('Mem Size:', numbersList, 'Generator converted to list')
+printSizeOf(numbers, 'Previous numbers generator')
+printSizeOf(squaredNumbers, 'Square numbers generator')
+printSizeOf(numbersList, 'Generator converted to list')
+
+
+from dataclasses import dataclass
+
+@dataclass
+class Counter:
+	code: str
+	start: int
+	end: int
+
+	counter = 0
+
+	def count(self):
+		if self.inRange():
+			self.counter += 1
+
+	inRange = lambda self: self.counter < self.end
+
+#Sample of a more complex generator, which generates objects and iterates upon a variable of theirs
+def counterCreator(codeStart, codeEnd, countStart, countEnd):
+	for code in range(ord(codeStart), ord(codeEnd)):
+		counter = Counter(chr(code), countStart, countEnd)
+		while counter.inRange():
+			counter.count()
+			yield counter
+
+counters = counterCreator('a', 'c', 1, 4)
+
+[print(id(c), repr(c), c.counter) for c in counters]
